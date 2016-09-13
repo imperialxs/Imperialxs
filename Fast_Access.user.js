12f14c2521a4959c024148fbe361cd14c32fd7f4
imperialxs <imperialord@gmail.com>
2016-09-13 11:15:26 -0500

    Update Fast_Access.user.js

--- Fast_Access.user.js
+++ Fast_Access.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name        Fast_Access3
-// @version      1.1
+// @name        Fast_Access_Final
+// @version      1.2
 // @description  Mat drop list (fast access to patterns) Drakor Wiki (Buttons)
 // @author      Lorinthar & Imperial
 // @match        http://*.drakor.com*
 // ==/UserScript==
 (function(){
-var FastButtons = '<a href="https://docs.google.com/spreadsheets/d/1P8vghr3Mnmxdx0ocY5Mwi6BYp5Dy4gf1VXTw1YK7dYk/edit#gid=1226207657" target="_new" class="navButton gs_topmenu_item gs_item_adventure">Mat Drop List</a>'+
-'<div id="load-tradeskills-Construction-patterns" class="navButton tradePatterns">Construction</div>'+
-'<div id="load-tradeskills-Inscription-patterns" class="navButton tradePatterns">Inscription</div>'+
-'<div id="load-tradeskills-Cooking-patterns" class="navButton tradePatterns">Cooking</div>'+
-'<div id="load-tradeskills-Jewelcrafting-patterns" class="navButton tradePatterns">Jewelcrafting</div>'+
-'<div id="load-tradeskills-Alchemy-patterns" class="navButton tradePatterns">Alchemy</div>'+
-'<div id="load-tradeskills-Smithing-patterns" class="navButton tradePatterns">Smithing</div>'+
-'<div id="load-tradeskills-Crafting-patterns" class="navButton tradePatterns">Crafting</div>'+
-'<a href="https://sites.google.com/site/drakorpedia/" target="_new"" class="navButton gs_topmenu_item gs_item_arena">Drakor Wiki</a>';
+var FastButtons = '<table align="center"><td style="background-color: #5e145b;"><div style="border:3px solid #d420cd"><a href="https://docs.google.com/spreadsheets/d/1P8vghr3Mnmxdx0ocY5Mwi6BYp5Dy4gf1VXTw1YK7dYk/edit#gid=1226207657" target="_new">Mat Drop List</a></div></td></body>'+
+'<td style="background-color: #5e145b;"><div style="border:3px solid #d420cd"> <a href="https://sites.google.com/site/drakorpedia/" target="_new">Drakor Wiki</a></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Construction-patterns" class="navButton tradePatterns">Construction</div></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Inscription-patterns" class="navButton tradePatterns">Inscription</div></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Cooking-patterns" class="navButton tradePatterns">Cooking</div></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Jewelcrafting-patterns" class="navButton tradePatterns">Jewelcrafting</div></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Alchemy-patterns" class="navButton tradePatterns">Alchemy</div></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Smithing-patterns" class="navButton tradePatterns">Smithing</div></td>'+
+'<td style="background-color: #9a6616;"><div id="load-tradeskills-Crafting-patterns" class="navButton tradePatterns">Crafting</div></td></table>';
 $(FastButtons).insertBefore('#gs_topmenu');
 })();
