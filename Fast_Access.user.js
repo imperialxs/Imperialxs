32f19ab7576b240cc1fb39f5a9a7d835b46eb2bf
imperialxs <imperialord@gmail.com>
2016-09-13 08:25:38 -0500

    Update Fast_Access.user.js

--- Fast_Access.user.js
+++ Fast_Access.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name        Fast_Access
-// @version      1.0
+// @name        Fast_Access3
+// @version      1.1
 // @description  Mat drop list (fast access to patterns) Drakor Wiki (Buttons)
 // @author      Lorinthar & Imperial
 // @match        http://*.drakor.com*
 // ==/UserScript==
 (function(){
-var FastButtons = '<a href="https://docs.google.com/spreadsheets/d/1P8vghr3Mnmxdx0ocY5Mwi6BYp5Dy4gf1VXTw1YK7dYk/edit#gid=1226207657" target="_new" class="bv_button linkRepair">Mat Drop List</a>'+
+var FastButtons = '<a href="https://docs.google.com/spreadsheets/d/1P8vghr3Mnmxdx0ocY5Mwi6BYp5Dy4gf1VXTw1YK7dYk/edit#gid=1226207657" target="_new" class="navButton gs_topmenu_item gs_item_adventure">Mat Drop List</a>'+
 '<div id="load-tradeskills-Construction-patterns" class="navButton tradePatterns">Construction</div>'+
 '<div id="load-tradeskills-Inscription-patterns" class="navButton tradePatterns">Inscription</div>'+
 '<div id="load-tradeskills-Cooking-patterns" class="navButton tradePatterns">Cooking</div>'+
@@ -14,6 +14,6 @@ var FastButtons = '<a href="https://docs.google.com/spreadsheets/d/1P8vghr3Mnmxd
 '<div id="load-tradeskills-Alchemy-patterns" class="navButton tradePatterns">Alchemy</div>'+
 '<div id="load-tradeskills-Smithing-patterns" class="navButton tradePatterns">Smithing</div>'+
 '<div id="load-tradeskills-Crafting-patterns" class="navButton tradePatterns">Crafting</div>'+
-'<a href="https://sites.google.com/site/drakorpedia/" target="_new"" class="navButton gs_topmenu_item gs_item_adventure">Drakor Wiki</a>';
-$(FastButtons).insertAfter('#bv_topbar');
+'<a href="https://sites.google.com/site/drakorpedia/" target="_new"" class="navButton gs_topmenu_item gs_item_arena">Drakor Wiki</a>';
+$(FastButtons).insertBefore('#gs_topmenu');
 })();
